f1040b1bb4b175b4d2730776cb90eca964ac7f5f
GuofengBai <bgf@qq.com>
2015-10-25 19:07:33 +0800

    1

--- LCMS/Client/src/main/java/org/businesslogicservice/staffblservice/StaffBLService.java
+++ LCMS/Client/src/main/java/org/businesslogicservice/staffblservice/StaffBLService.java
@@ -1,5 +1,10 @@
 package org.businesslogicservice.staffblservice;
 
+import org.po.ResultMessage;
+import org.vo.StaffVO;
+
 public interface StaffBLService {
+	
+	public ResultMessage newStaff(StaffVO vo);
 
 }
