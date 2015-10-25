b7b43d7e39a218d6f0a0a97953f1bc3425735be0
GuofengBai <bgf@qq.com>
2015-10-25 14:56:08 +0800

    包的创建

---
+++ LCMS/Client/src/main/java/org/po/StaffPO.java
@@ -0,0 +1,23 @@
+package org.po;
+
+import java.io.Serializable;
+
+public class StaffPO implements Serializable{
+	
+	public UserPO acount;
+	public String name;
+	public String num;
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StaffPO(){
+		super();
+	}
+	
+	public
+
+}
