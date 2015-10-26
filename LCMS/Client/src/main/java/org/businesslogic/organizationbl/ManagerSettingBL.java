64468d80d6a9b14262369890b81b54cf3ddd23fc
Interstellar <georgecurz@qq.com>
2015-10-26 16:56:24 +0800

    axy

--- LCMS/Client/src/main/java/org/businesslogic/organizationbl/ManagerSettingBL.java
+++ LCMS/Client/src/main/java/org/businesslogic/organizationbl/ManagerSettingBL.java
@@ -1,9 +1,14 @@
 package org.businesslogic.organizationbl;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.businesslogicservice.organizationblservice.ManagerSettingBLService;
 
 public class ManagerSettingBL implements ManagerSettingBLService{
-
+	String[] cities={"北京","广州","上海","南京"};
 	public boolean ManagerSetCitiesDistance(int city1, int city2,
 			double distance) {
 		// TODO Auto-generated method stub
@@ -19,5 +24,13 @@ public class ManagerSettingBL implements ManagerSettingBLService{
 		// TODO Auto-generated method stub
 		return false;
 	}
+	//public void initDistances() throws IOException{
+	//	File file = new File("TxtData/Distances.txt");
+	//	BufferedWriter bw=new BufferedWriter(new FileWriter("TxtData/Distances.txt"));
+	//	for(int i=0;i<cities.length;i++){
+	//		bw.write(cities[i]+" ");
+	//	}
+		
+	//}
 
 }
