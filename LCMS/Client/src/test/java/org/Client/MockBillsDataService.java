8757b327b4ae93257a632985984207e5988fb8ff
Interstellar <georgecurz@qq.com>
2015-11-16 18:49:43 +0800

    MockBillsDataService
    rt

--- LCMS/Client/src/test/java/org/Client/MockBillsDataService.java
+++ LCMS/Client/src/test/java/org/Client/MockBillsDataService.java
@@ -1,14 +1,51 @@
 package org.Client;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import org.po.BillsPO;
 import org.po.ResultMessage;
 
 public class MockBillsDataService {
+	public ArrayList<BillsPO> billsList;
+
 	public ResultMessage addBills(BillsPO po) throws RemoteException {
 		// TODO Auto-generated method stub
-		ResultMessage rm=new ResultMessage(true,null);
-		return rm;
+		
+		String fileName="SerializableData/IB.file";
+		System.out.println(fileName);
+		try {
+			FileInputStream fis = new FileInputStream(fileName);
+	        BufferedInputStream bis = new BufferedInputStream(fis);  
+	        ObjectInputStream ois = new ObjectInputStream(bis);  
+	        billsList= (ArrayList<BillsPO>) ois.readObject();
+	        billsList.add(po);
+            FileOutputStream fos = new FileOutputStream(fileName);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(billsList);
+            ois.close();
+            oos.close();
+            System.out.println("添加成功！");
+            String[] info={"add bills successfully!!"};
+            return new ResultMessage(true,info);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ResultMessage re=new ResultMessage(true,null);
+		return re;
 	}
 }
--- LCMS/Server/src/main/java/org/data/commoditydata/CommodityData.java
+++ LCMS/Server/src/main/java/org/data/commoditydata/CommodityData.java
@@ -1,14 +1,24 @@
 package org.data.commoditydata;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 import org.dataservice.commoditydataservice.CommodityDataService;
+import org.po.BillsPO;
 import org.po.ComPO;
 import org.po.ResultMessage;
 
 public class CommodityData extends UnicastRemoteObject implements CommodityDataService{
 
+	public ArrayList<ComPO> ComList;
 	protected CommodityData() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
@@ -17,6 +27,7 @@ public class CommodityData extends UnicastRemoteObject implements CommodityDataS
 	public ResultMessage addCom(ComPO po) {
 		// TODO Auto-generated method stub
 		return null;
+
 	}
 
 	public ResultMessage delCom(ComPO po) {
