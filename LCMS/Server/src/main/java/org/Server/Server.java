ef3cadcae5439d67d56c76017e05a586af509918
Interstellar <georgecurz@qq.com>
2015-12-09 20:18:40 +0800

    rt

--- LCMS/Server/src/main/java/org/Server/Server.java
+++ LCMS/Server/src/main/java/org/Server/Server.java
@@ -1,9 +1,14 @@
 package org.Server;
 
+import javax.swing.JOptionPane;
+
 public class Server 
 {
     public static void main( String[] args )
     {
         RMIHelper.init();
+        
+        JOptionPane.showMessageDialog(null,"运行Server成功",null ,
+				JOptionPane.WARNING_MESSAGE);
     }
 }
